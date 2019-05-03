b28997de71a57d344ed15022514f5abfe8759549
shoi <shoi.itou@gmail.com>
2019-05-03 17:06:54 +0900

    ARKitの導入

---
+++ jackhack2019B/Assets/Scripts/BubbleGenerator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class BubbleGenerator : MonoBehaviour {
+  [SerializeField] Bubble bubblePrefab;
+
+  void Update () {
+    if (Input.GetButtonDown("Fire1"))
+    {
+      Instantiate(bubblePrefab, transform.position, Quaternion.identity);
+    }
+  }
+}
