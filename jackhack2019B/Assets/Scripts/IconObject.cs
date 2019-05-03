5eed2a772fe9c5e05453b54af5fa9e9dfe8a6a51
shoi <shoi.itou@gmail.com>
2019-05-03 23:02:51 +0900

    図鑑作り途中

---
+++ jackhack2019B/Assets/Scripts/IconObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenProfile(){
+        Application.OpenURL("https://jack-app.slack.com/team/U7H6X9ETW");
+    }
+}
