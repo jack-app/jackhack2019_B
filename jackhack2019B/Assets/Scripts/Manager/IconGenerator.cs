8312b39597c8702a548da64523c68d03f3e2028c
shoi <uranun313@gmail.com>
2019-05-04 00:57:07 +0900

    デッキのアイコンとかを作った

---
+++ jackhack2019B/Assets/Scripts/Manager/IconGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IconGenerator : MonoBehaviour
+{
+    public GameObject IconPrefab;
+    public GameObject Content;
+
+    private PocketHumanDataSet pocketHumanDataSet;
+    private PocketHumanData[] pocketHumanDatas;
+    
+    void Start()
+    {
+        pocketHumanDataSet = Resources.Load<PocketHumanDataSet>("PocketHumanDataSet");
+        pocketHumanDatas = pocketHumanDataSet.PocketHumanDatas;
+        MakeIcons();
+    }
+
+    void MakeIcons()
+    {
+        foreach (var pocketHuman in UserDataManager.LoadAllPocketHuman())
+        {
+            //レベルが0じゃなかったらアイコンを作る(Keyに名前、Valueにレベルが入っている
+            if (pocketHuman.Value == 0)
+            {
+                GameObject obj = Instantiate(IconPrefab, Content.transform);
+                obj.GetComponent<Image>().sprite = FindPocketHumanData(pocketHuman.Key).Icon;
+            }
+        }
+    }
+
+    PocketHumanData FindPocketHumanData(string human_name)
+    {
+        foreach (var pocketHumanData in pocketHumanDatas)
+        {
+            if (pocketHumanData.Name == human_name)
+            {
+                return pocketHumanData;
+            }
+        }
+
+        return null;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ jackhack2019B/Assets/Scripts/Manager/UserDataManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserDataManager : MonoBehaviour
+{
+    public static void SavePocketHuman(string human_name)
+    {
+        int level = LoadPocketHuman(human_name);
+        //ポケットヒューマンのレベルをセーブ
+        PlayerPrefs.SetInt("HumanLevel" + human_name, level + 1);
+    }
+
+    public static int LoadPocketHuman(string human_name)
+    {
+        return PlayerPrefs.GetInt("HumanLevel" + human_name, 0);
+    }
+
+    public static Dictionary<string, int> LoadAllPocketHuman()
+    {
+        Dictionary<string, int> resultDic = new Dictionary<string, int>();
+        
+        PocketHumanDataSet pocketHumanDataSet = Resources.Load<PocketHumanDataSet>("PocketHumanDataSet");
+        PocketHumanData[] pocketHumanDatas = pocketHumanDataSet.PocketHumanDatas;
+
+        foreach (var pocketHumanData in pocketHumanDatas)
+        {
+            resultDic[pocketHumanData.Name] = LoadPocketHuman(pocketHumanData.Name);
+        }
+        
+        return resultDic;
+    }
+}
