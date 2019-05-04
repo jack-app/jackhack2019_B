13063d547b7a6cadd36b7c08887c7a631ceca3b1
shoi <shoi.itou@gmail.com>
2019-05-04 12:08:11 +0900

    顔を認識できた！

--- jackhack2019B/Assets/Scripts/Manager/ScenePanelManager.cs
+++ jackhack2019B/Assets/Scripts/Manager/ScenePanelManager.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Enums;
+using UnityEngine.UI;
 
 public class ScenePanelManager : MonoBehaviour
 {
     public GameObject DeckScenePanel;
     public GameObject CardScenePanel;
 
+    public Image CardImage;
+
+    public void ChangerScenePanelToCamera(){
+        AllSetActiveFalse();
+    }
+
+    public void ChangeScenePanelToDeck(){
+        AllSetActiveFalse();
+        DeckScenePanel.SetActive(true);
+    }
+
     public void ChangeScenePanelToCard(PocketHumanData pocketHumanData)
     {
         AllSetActiveFalse();
-        
+        CardScenePanel.SetActive(true);
+        CardImage.sprite = pocketHumanData.Card;
     }
 
     void AllSetActiveFalse()
